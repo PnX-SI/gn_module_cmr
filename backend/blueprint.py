5a3c24b0548022e822bfac2a6a8571f2e4f967de
fred.lpo07 <frederic.cloitre@lpo.fr>
2018-09-25 00:35:42 +0200

    ébauche de post pour program

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -1,8 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from geonature.utils.env import DB
 from geonature.utils.utilssqlalchemy import json_resp
 from .models import TPrograms
+from pypnusershub import routes as fnauth
 
 blueprint = Blueprint('cmr', __name__)
 
@@ -15,3 +16,16 @@ def test():
 def get_programs():
     pgs = DB.session.query(TPrograms).all()
     return [pg.as_dict() for pg in pgs]
+
+@blueprint.route('/programs', methods=['POST'])
+@fnauth.check_auth_cruved('R', False, id_app=3)
+@json_resp
+def post_programs():
+    """ Ajout d'un programme"""
+    # TODO : Validation de la donnée
+    data = dict(request.get_json())
+    newprog = TPrograms(**data)
+    DB.session.add(newprog)
+    DB.session.commit()
+    DB.session.flush()
+    return newprog.as_dict()
