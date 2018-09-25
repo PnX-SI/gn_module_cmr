b1cffda5f713f94d33012e79f3a6aec10d6e2b5d
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2018-09-25 12:47:05 +0200

    Formulaire saisir/modifier programes

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -1,17 +1,71 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
-from geonature.utils.env import DB
+from geonature.utils.errors import GeonatureApiError
+from geonature.utils.env import DB, get_module_id
 from geonature.utils.utilssqlalchemy import json_resp
+
+from pypnusershub import routes as fnauth
+
 from .models import TPrograms
 
 blueprint = Blueprint('cmr', __name__)
+
+try:
+    ID_MODULE = get_module_id('cmr')
+except Exception as e:
+    ID_MODULE = 'Error'
 
 @blueprint.route('/test', methods=['GET', 'POST'])
 def test():
     return 'It works'
 
 @blueprint.route('/programs', methods=['GET'])
+@fnauth.check_auth_cruved('R', False, id_app=ID_MODULE)
 @json_resp
 def get_programs():
     pgs = DB.session.query(TPrograms).all()
     return [pg.as_dict() for pg in pgs]
+
+@blueprint.route('/programs/<int:id_program>', methods=['GET'])
+@fnauth.check_auth_cruved('R', False, id_app=ID_MODULE)
+@json_resp
+def get_program_by_id(id_program):
+    pg = (DB.session.query(TPrograms)
+        .filter(TPrograms.id_program == id_program)
+        .one()
+    )
+    return pg.as_dict()
+
+@blueprint.route('/programs', methods=['POST'])
+@blueprint.route('/programs/<int:id_program>', methods=['POST'])
+@fnauth.check_auth_cruved('C', False, id_app=ID_MODULE)
+@json_resp
+def post_programs(id_program = None):
+    """ 
+        Ajout d'un programme (program name unique)
+    """
+    data = dict(request.get_json())
+    program = data['program_name']
+    
+    newpg = TPrograms(**data)
+
+    exists = (
+        DB.session.query(TPrograms)
+        .filter(TPrograms.program_name == program)
+        .all()
+    )
+    if exists:
+        if not exists[0].id_program == id_program :
+            raise GeonatureApiError('This program already exists')
+
+    if newpg.id_program:
+        DB.session.merge(newpg)
+    else:
+        DB.session.add(newpg)
+
+    try:
+        DB.session.commit()
+    except:
+        raise GeonatureApiError('Cannot create program')
+    
+    return newpg.as_dict(True)
--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { GN2CommonModule } from "@geonature_common/GN2Common.module";
 import { Routes, RouterModule } from "@angular/router";
-import { ProgramsComponent } from "./components/programs.component";
+import { ProgramsListComponent } from "./components/programs.list.component";
+import { ProgramsFormComponent } from "./components/programs.form.component";
 
 // my module routing
-const routes: Routes = [{ path: "", component: ProgramsComponent }];
+const routes: Routes = [
+  { path: "", component: ProgramsListComponent },
+  { path: "form_pg", component: ProgramsFormComponent },
+  { path: 'form_pg/:id', component: ProgramsFormComponent },
+];
 
 @NgModule({
-  declarations: [ProgramsComponent],
+  declarations: [ProgramsListComponent, ProgramsFormComponent],
   imports: [GN2CommonModule, RouterModule.forChild(routes), CommonModule],
   providers: [],
   bootstrap: []
