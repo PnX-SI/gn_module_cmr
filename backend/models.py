584469924bdfaf0392eae5f03397d8239d09cd12
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-09-25 10:00:13 +0200

    models TIndividuals

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy import ForeignKey
+
 from geonature.utils.utilssqlalchemy import serializable
 from geonature.utils.env import DB
 
@@ -11,3 +13,24 @@ class TPrograms(DB.Model):
     )
     program_name = DB.Column(DB.Unicode)
     program_desc = DB.Column(DB.Unicode)
+
+
+@serializable
+class TIndividuals(DB.Model):
+    __tablename__ = 't_individuals'
+    __table_args__ = {'schema': 'pr_cmr'}
+    id_individual = DB.Column(
+        DB.Integer,
+        primary_key=True,
+    )
+    cd_nom = DB.Column(DB.Integer)
+    tag_code = DB.Column(DB.Unicode)
+    tag_location = DB.Column(DB.Unicode)
+    id_site_tag = DB.Column( 
+        DB.Integer,
+        ForeignKey('gn_monitoring.t_base_sites.id_base_site')
+    )
+    id_nomenclature_sex = DB.Column(
+        DB.Integer,
+        ForeignKey('ref_nomenclatures.t_nomenclatures.id_nomenclature')
+    )
