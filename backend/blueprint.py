fdfe09bdd2460873142fe42df1bad6e981981e5f
fred.lpo07 <frederic.cloitre@lpo.fr>
2018-09-25 09:37:37 +0200

    add post request for programs

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -1,17 +1,50 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from pypnusershub import routes as fnauth
 
-from geonature.utils.env import DB
+from geonature.utils.env import DB, get_module_id
+from geonature.utils.errors import GeonatureApiError
 from geonature.utils.utilssqlalchemy import json_resp
 from .models import TPrograms
 
 blueprint = Blueprint('cmr', __name__)
 
+try:
+    ID_MODULE = get_module_id('cmr')
+except Exception as e:
+    ID_MODULE = 'Error'
+
 @blueprint.route('/test', methods=['GET', 'POST'])
 def test():
     return 'It works'
+
 
 @blueprint.route('/programs', methods=['GET'])
 @json_resp
 def get_programs():
     pgs = DB.session.query(TPrograms).all()
     return [pg.as_dict() for pg in pgs]
+
+
+@blueprint.route('/programs', methods=['POST'])
+@fnauth.check_auth_cruved('R', False, id_app=ID_MODULE)
+@json_resp
+def post_programs():
+    """ Ajout d'un programme (program name unique)"""
+    data = dict(request.get_json())
+    progname = data['program_name']
+    if len(progname) >= 1:
+        exists = DB.session.query(DB.exists().where(TPrograms.program_name == progname)).scalar()
+        if exists:
+            raise GeonatureApiError('This program already exists')
+        else:
+            try:
+                newprog = TPrograms(**data)
+            except:
+                raise GeonatureApiError('Cannot create program')
+    else:
+        raise GeonatureApiError('Program name empty')
+
+    DB.session.add(newprog)
+    DB.session.commit()
+    DB.session.flush()
+    return newprog.as_dict()
--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,16 @@
-from geonature.utils.utilssqlalchemy import serializable
+from geoalchemy2 import Geometry
+
 from geonature.utils.env import DB
+from geonature.utils.utilssqlalchemy import serializable
+from flask import current_app
+from sqlalchemy.dialects.postgresql import UUID
+
+
+local_srid = current_app.config['LOCAL_SRID']
 
 @serializable
 class TPrograms(DB.Model):
+    """Table des protocoles de CMR du module"""
     __tablename__ = 't_programs'
     __table_args__ = {'schema': 'pr_cmr'}
     id_program = DB.Column(
@@ -11,3 +19,30 @@ class TPrograms(DB.Model):
     )
     program_name = DB.Column(DB.Unicode)
     program_desc = DB.Column(DB.Unicode)
+
+@serializable
+class TOperations(DB.Model):
+    """Table des opérations sur un individu"""
+    __tablename__ = 't_operations'
+    __table_args__ = {'schema': 'pr_cmr'}
+    id_operation = DB.Column(
+        DB.Integer,
+        primary_key=True
+    )
+    id_individual = DB.Column(DB.Integer,
+                              nullable=False)
+    id_site = DB.Column(
+        DB.Integer,
+        nullable=False
+    )
+    geom_point_4326 = DB.Column(Geometry(geometry_type='POINT', srid=4326))
+    geom_point_local = DB.Column(Geometry(geometry_type='POINT', srid=local_srid))
+    date_min = DB.Column(DB.TIMESTAMP, nullable=False)
+    date_max = DB.Column(DB.TIMESTAMP, nullable=False)
+    id_nomenclature_cmr_action = DB.Column(DB.Integer)
+    id_nomenclature_obs_method = DB.Column(DB.Integer)
+    id_nomenclature_life_stage = DB.Column(DB.Integer)
+    id_nomenclature_bio_condition = DB.Column(DB.Integer)
+    id_nomenclature_determination_method = DB.Column(DB.Integer)
+    determiner = DB.Column(DB.Unicode)
+    unique_id_sinp = DB.Column(UUID(as_uuid=True), nullable=False)
