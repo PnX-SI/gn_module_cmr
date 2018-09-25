3dfdae5d60debc1e304cb06d8b03c32b35d07618
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-09-25 11:26:29 +0200

    Add sex to Json response
    Join with TNomenclatures table.

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -2,6 +2,9 @@ from flask import Blueprint
 
 from geonature.utils.env import DB
 from geonature.utils.utilssqlalchemy import json_resp
+
+from pypnnomenclature.models import TNomenclatures
+
 from .models import TPrograms, TIndividuals, Taxonomie
 
 blueprint = Blueprint('cmr', __name__)
@@ -20,13 +23,14 @@ def get_programs():
 @blueprint.route('/individuals', methods=['GET'])
 @json_resp
 def get_individuals():
-    q = DB.session.query(TIndividuals, Taxonomie.nom_complet, Taxonomie.nom_vern).join(
-        Taxonomie, Taxonomie.cd_nom == TIndividuals.cd_nom)
+    q = DB.session.query(TIndividuals, Taxonomie.nom_complet, Taxonomie.nom_vern, TNomenclatures.mnemonique).join(
+        Taxonomie, Taxonomie.cd_nom == TIndividuals.cd_nom).join(
+            TNomenclatures, TNomenclatures.id_nomenclature == TIndividuals.id_nomenclature_sex)
     data = q.all()
     res = []
-    for ind, nom_complet, nom_vern in data:
+    for ind, nom_complet, nom_vern, sex in data:
         d = ind.as_dict()
-        d.update({'nom_complet': nom_complet, 'nom_vern': nom_vern})
+        d.update({'nom_complet': nom_complet, 'nom_vern': nom_vern, 'sexe': sex})
         res.append(d)
     return res
 
