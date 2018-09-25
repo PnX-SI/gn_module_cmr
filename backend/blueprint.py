6c2dbee87632b58ac81cad4de4dd15996dc7a8db
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-09-25 10:02:52 +0200

    Add route to list individuals
    Returns a JSON array with individuals.

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 from geonature.utils.env import DB
 from geonature.utils.utilssqlalchemy import json_resp
-from .models import TPrograms
+from .models import TPrograms, TIndividuals
 
 blueprint = Blueprint('cmr', __name__)
 
@@ -15,3 +15,10 @@ def test():
 def get_programs():
     pgs = DB.session.query(TPrograms).all()
     return [pg.as_dict() for pg in pgs]
+
+
+@blueprint.route('/individuals', methods=['GET'])
+@json_resp
+def get_individuals():
+    data = DB.session.query(TIndividuals).all()
+    return [d.as_dict() for d in data]
